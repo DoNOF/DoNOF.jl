c6c43a188bf5eb3ccbc4e9ecff66678b45d29011
Juan Felipe Huan Lew Yee <38631029+felipelewyee@users.noreply.github.com>
2026-02-03 19:53:34 -0600

    Implement geometry optimization in optgeo function
    Integrate optimization process using the optimize function with energy and gradient calculations.

--- src/optgeo.jl
+++ src/optgeo.jl
@@ -4,29 +4,31 @@ const ANGSTROM_TO_BOHR = 1.8897261246
 function optgeo(mol::String, bset_ref, p_ref)
 
     # Move coordinates so center of mass is at origin
+    mt, cm = center_of_mass(bset_ref)
+    spin_mult, symbols, coords = string_to_xyz(mol, cm)
+    mol_at_cm = xyz_data_to_string(spin_mult, symbols, coords)
 
     # Reference Single Point
+    bset, _ = DoNOF.molecule(mol_at_cm, bset_ref.name, spherical = true)
+    p = deepcopy(p_ref)
+    E, C_ref, n_ref = DoNOF.energy(bset, p)
 
     # Optimize geometry (coords in bohr)
     coords = coords .* ANGSTROM_TO_BOHR
-    ###### INSERTAR OPTIMIZACION ######
-    # ENTRA: COORDS
-    # OPTIMIZE
-    # ENERGIA
-        #c -> g_linearized(c, symbols, spin_mult, C_ref, n_ref, bset, p_ref)
-    # GRADIENTE
-        #c -> e_linearized(c, symbols, spin_mult, C_ref, n_ref, bset, p_ref)
-    # SALE: RES.minimizer
+    res = optimize(
+        c -> e_linearized(c, symbols, spin_mult, C_ref, n_ref, bset, p_ref),
+        c -> g_linearized(c, symbols, spin_mult, C_ref, n_ref, bset, p_ref),
+        coords,
+        LBFGS(),
+        Optim.Options(g_abstol = 10^-4),
+        inplace = false,
+    )
     
     # coords de Bohr a Angstrom
     coords = res.minimizer * BOHR_TO_ANGSTROM
     coords[abs.(coords) .< 10^-4] .= 0
+    println(coords)
 
-    # convertir datos a string de xyz
-    # ej.
-    # spin_mult: "0 1"
-    # symbols: ["H", "H"]
-    # coords: [x1, y1, z1, x2, y2, z2]
     mol_optimized = xyz_data_to_string(spin_mult, symbols, coords)
 
     return mol_optimized
